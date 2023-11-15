955b531cb86ed5be19dfe2ff3e17df48f46eb585
TienDoann <102640653+TienDoann@users.noreply.github.com>
2023-11-15 08:54:43 +0700

    fix Build project

--- src/app/pages/course/course.component.ts
+++ src/app/pages/course/course.component.ts
@@ -10,5 +10,61 @@ import {CourseCardComponent} from "../components/course-card/course-card.compone
   styleUrl: './course.component.css'
 })
 export class CourseComponent {
-
+  data = [
+    {
+      id: 1,
+      name: 'Đoàn Liêng tiến',
+      subtitle: 'Công nghệ thông tin',
+      text: 'asdfasdfasmgndfkbglsbafsadf1sa4f6sa1f3a5s4d6fas1df334tq4rqwasdfasdfasmgndfkbglsbafsadf1sa4f6sa1f3a5s4d6fas1df334tq4rqw',
+      image: 'assets/images/course/cs-img1.jpg',
+      course: '3 years',
+      classSize: '20',
+      Duration: '10 Hours',
+      price: '999$'
+    },
+    {
+      id: 2,
+      name: 'Đoàn Liêng tiến',
+      subtitle: 'Công nghệ thông tin',
+      text: 'asdfasdfasmgndfkbglsbafsadf1sa4f6sa1f3a5s4d6fas1df334tq4rqwasdfasdfasmgndfkbglsbafsadf1sa4f6sa1f3a5s4d6fas1df334tq4rqw',
+      image: 'assets/images/course/cs-img2.jpg',
+      course: '3 years',
+      classSize: '20',
+      Duration: '10 Hours',
+      price: '999$'
+    },
+    {
+      id: 3,
+      name: 'Đoàn Liêng tiến',
+      subtitle: 'Công nghệ thông tin',
+      text: 'asdfasdfasmgndfkbglsbafsadf1sa4f6sa1f3a5s4d6fas1df334tq4rqwasdfasdfasmgndfkbglsbafsadf1sa4f6sa1f3a5s4d6fas1df334tq4rqw',
+      image: 'assets/images/course/cs-img3.jpg',
+      course: '3 years',
+      classSize: '20',
+      Duration: '10 Hours',
+      price: '999$'
+    },
+    {
+      id:4,
+      name: 'Đoàn Liêng tiến',
+      subtitle: 'Công nghệ thông tin',
+      text: 'asdfasdfasmgndfkbglsbafsadf1sa4f6sa1f3a5s4d6fas1df334tq4rqwasdfasdfasmgndfkbglsbafsadf1sa4f6sa1f3a5s4d6fas1df334tq4rqw',
+      image: 'assets/images/course/cs-img4.jpg',
+      course: '3 years',
+      classSize: '20',
+      Duration: '10 Hours',
+      price: '999$'
+    },
+    {
+      id: 5,
+      name: 'Đoàn Liêng tiến',
+      subtitle: 'Công nghệ thông tin',
+      text: 'asdfasdfasmgndfkbglsbafsadf1sa4f6sa1f3a5s4d6fas1df334tq4rqwasdfasdfasmgndfkbglsbafsadf1sa4f6sa1f3a5s4d6fas1df334tq4rqw',
+      image: 'assets/images/course/cs-img5.jpg',
+      course: '3 years',
+      classSize: '20',
+      Duration: '10 Hours',
+      price: '999$'
+    },
+  ];
 }
